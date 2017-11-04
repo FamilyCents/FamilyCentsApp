68574c451578478ac6a9ec1f6ffd676c4173eb81
Josh Wilson <sohjsolwin@gmail.com>
2017-11-04 19:38:34 -0400

    Added Utilization Score

--- source/FamilyCents.App.CreditEngine/ScoreComponents/UtilizationScore.cs
+++ source/FamilyCents.App.CreditEngine/ScoreComponents/UtilizationScore.cs
@@ -11,6 +11,45 @@ namespace FamilyCents.App.CreditEngine.ScoreComponents
   /// </summary>
   public class UtilizationScore : IScoreComponent
   {
-    public double Score { get => 0; }
+    private const double _maxScore = 1000;
+
+    private const int _maxPercentForMax = 30;
+
+    public double Score { get => CalculateScore(this.CurrentBalance); }
+
+    public double CreditLimit { get; private set; }
+    public double CurrentBalance { get; private set; }
+
+    public UtilizationScore(double creditLimit, double currentBalance)
+    {
+      this.CreditLimit = creditLimit;
+      this.CurrentBalance = currentBalance;
+    }
+
+    private double CalculateScore(double currentBalance)
+    {
+      var result = 0d;
+
+      var utilization = Math.Round(currentBalance / this.CreditLimit, 2, MidpointRounding.AwayFromZero); //Credit utilization percentage.
+
+      if (utilization <=0.30)
+      {
+        result = _maxScore;
+      }
+      else if (utilization > 0.30 && utilization < 1) //55 point range. 
+      {
+        var utilPercent = (Math.Round((utilization - 0.30) / (0.70), 2, MidpointRounding.AwayFromZero));
+
+        var logPercent = Math.Pow(0.8, (utilPercent * 100)/20);
+
+        result = (logPercent) * _maxScore;
+      }
+      else // > 0.85
+      {
+        result = 0;
+      }
+      
+      return Math.Round(result, 2, MidpointRounding.AwayFromZero);
+    }
   }
 }
--- tests/FamilyCents.App.Tests/CreditScoreTests.cs
+++ tests/FamilyCents.App.Tests/CreditScoreTests.cs
@@ -8,6 +8,8 @@ namespace FamilyCents.App.Tests
 {
   public class CreditScoreTests
   {
+
+    #region Lifespan
     [Fact]
     public void CalculateLifeSpanScore_8yearsMaxScore()
     {
@@ -97,5 +99,178 @@ namespace FamilyCents.App.Tests
 
       Assert.Equal(861, score);
     }
+
+    #endregion
+
+    #region Utilization
+
+    [Fact]
+    public void CalculateUtilization_All()
+    {
+
+      var utilization = new UtilizationScore(10000, 10000);
+
+      var score = utilization.Score;
+
+      Assert.Equal(0, score);
+    }
+
+    [Fact]
+    public void CalculateUtilization_10percent()
+    {
+
+      var utilization = new UtilizationScore(10000, 1000);
+
+      var score = utilization.Score;
+
+      Assert.Equal(1000, score);
+    }
+
+    [Fact]
+    public void CalculateUtilization_20percent()
+    {
+
+      var utilization = new UtilizationScore(10000, 2000);
+
+      var score = utilization.Score;
+
+      Assert.Equal(1000, score);
+    }
+
+    [Fact]
+    public void CalculateUtilization_30percent()
+    {
+
+      var utilization = new UtilizationScore(10000, 3000);
+
+      var score = utilization.Score;
+
+      Assert.Equal(1000, score);
+    }
+
+    [Fact]
+    public void CalculateUtilization_31percent()
+    {
+
+      var utilization = new UtilizationScore(10000, 3100);
+
+      var score = utilization.Score;
+
+      Assert.Equal(988.9, score);
+    }
+
+    [Fact]
+    public void CalculateUtilization_40percent()
+    {
+
+      var utilization = new UtilizationScore(10000, 4000);
+
+      var score = utilization.Score;
+
+      Assert.Equal(855.39, score);
+    }
+
+    [Fact]
+    public void CalculateUtilization_50percent()
+    {
+
+      var utilization = new UtilizationScore(10000, 5000);
+
+      var score = utilization.Score;
+
+      Assert.Equal(723.57, score);
+    }
+
+    [Fact]
+    public void CalculateUtilization_60percent()
+    {
+
+      var utilization = new UtilizationScore(10000, 6000);
+
+      var score = utilization.Score;
+
+      Assert.Equal(618.93, score);
+    }
+
+    [Fact]
+    public void CalculateUtilization_70percent()
+    {
+
+      var utilization = new UtilizationScore(10000, 7000);
+
+      var score = utilization.Score;
+
+      Assert.Equal(529.43, score);
+    }
+
+    [Fact]
+    public void CalculateUtilization_80percent()
+    {
+
+      var utilization = new UtilizationScore(10000, 8000);
+
+      var score = utilization.Score;
+
+      Assert.Equal(452.87, score);
+    }
+
+    [Fact]
+    public void CalculateUtilization_84percent()
+    {
+
+      var utilization = new UtilizationScore(10000, 8400);
+
+      var score = utilization.Score;
+
+      Assert.Equal(423.54, score);
+    }
+
+    [Fact]
+    public void CalculateUtilization_90percent()
+    {
+
+      var utilization = new UtilizationScore(10000, 9000);
+
+      var score = utilization.Score;
+
+      Assert.Equal(383.08, score);
+    }
+
+    [Fact]
+    public void CalculateUtilization_95percent()
+    {
+
+      var utilization = new UtilizationScore(10000, 9500);
+
+      var score = utilization.Score;
+
+      Assert.Equal(354.3, score);
+    }
+
+    [Fact]
+    public void CalculateUtilization_98percent()
+    {
+
+      var utilization = new UtilizationScore(10000, 9800);
+
+      var score = utilization.Score;
+
+      Assert.Equal(338.83, score);
+    }
+
+    [Fact]
+    public void CalculateUtilization_99percent()
+    {
+
+      var utilization = new UtilizationScore(10000, 9900);
+
+      var score = utilization.Score;
+
+      Assert.Equal(331.36, score);
+    }
+
+
+
+    #endregion
   }
 }
