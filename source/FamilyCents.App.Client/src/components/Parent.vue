c87f898b94323bf43d0c6b045d56be22a4288650
Justin Fenn <jrfenn1824@gmail.com>
2017-11-05 02:31:06 -0500

    Finish all functionality except user update functions

--- source/FamilyCents.App.Client/src/components/Parent.vue
+++ source/FamilyCents.App.Client/src/components/Parent.vue
@@ -1,28 +1,80 @@
 <template>
   <div class="parent">
       <div>
-        <ul>
-          <li v-for="(child, index) in children" :key="index">
-            <router-link :to="`/account/${child.customerId}`" >{{child.name}}</router-link>      
-          </li>
-        </ul>
+        <v-layout row wrap>
+            <v-flex
+              xs4
+              v-for="(child, index) in children"
+              :key="index"
+            >
+              <v-card >
+                <router-link :to="`/account/${child.customerId}`" >{{child.name}}</router-link>:
+                <p>Balance: {{child.virtualBalance}}</p>
+                <p>Credit Remaining: {{creditRemaining(child)}}</p>
+                <p>Credit Score: {{child.virtualCreditScore}}</p>
+              </v-card>
+            </v-flex>
+          </v-layout>
     </div>
-    <!-- <TaskList></TaskList> -->
+    <TaskList></TaskList>
+    
+    <v-btn
+      color="green"
+      dark
+      fixed
+      right
+      bottom
+      fab
+      @click.native.stop="addNewDialog = true"
+    >
+      <v-icon>add</v-icon>
+    </v-btn>
+    <v-dialog absolute v-model="addNewDialog">
+      <v-card>
+        <v-card-text>
+          <v-text-field label="Task Description" v-model="newTaskDescription"></v-text-field>
+          <v-text-field label="Reward" type="number" v-model="newTaskValue"></v-text-field>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn flat color="primary" @click.native="createTask">Submit</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </div>
 </template>
 
 <script>
-
+import TaskList from './TaskList'
 export default {
   name: 'Parent',
-
-  // data () {
-  //   return {
-  //   }
-  // },
+  data() {
+    return {
+      addNewDialog: false,
+      newTaskDescription: "",
+      newTaskValue: 0
+    }
+  },
+  components: {
+    TaskList
+  },
   computed: {
     children() {
       return this.$store.getters.family.filter(m => !m.IsPrimary);
+    }
+  },
+  methods: {
+    creditRemaining(user){
+      return user.virtualCreditLimit - user.virtualBalance;
+    },
+    createTask(){
+      this.$store.dispatch('createFamilyTask', {
+          description: this.newTaskDescription,
+          value: this.newTaskValue
+        });
+      this.addNewDialog = false;
+      this.newTaskDescription = "";
+      this.newTaskValue = 0;
     }
   }
 }
--- source/FamilyCents.App.Client/src/components/TaskList.vue
+++ source/FamilyCents.App.Client/src/components/TaskList.vue
@@ -5,30 +5,24 @@
         <div>
         <v-list two-line subheader>
           <v-subheader>Tasks</v-subheader>
-          <v-list-tile avatar>
+          <v-list-tile v-for="(task, index) in taskList" :key="index">
             <v-list-tile-action>
-              <v-checkbox v-model="notifications"></v-checkbox>
+              <v-checkbox 
+              v-model="task.completedBy"
+              @click="completeTask(task.id)"
+              ></v-checkbox>
+              <v-checkbox 
+              v-model="task.approvedBy"
+              @click="approveTask(task.id)"
+              v-if="isParent"></v-checkbox>
             </v-list-tile-action>
             <v-list-tile-content>
-              <v-list-tile-title>Notifications</v-list-tile-title>
-              <v-list-tile-sub-title>Allow notifications</v-list-tile-sub-title>
+              <v-list-tile-title>{{task.description}} - ${{task.value}}</v-list-tile-title>
             </v-list-tile-content>
           </v-list-tile>
         </v-list>
-            <div headline>
-              Tasks
-            </div>
-            <div>
-                <ul>
-                  <li v-for="(task, index) in taskList" :key="index">
-                    <div> Task: {{task.description}}</div>
-                    <div> Reward: {{task.reward}}</div>        
-                  </li>
-                </ul>
-            </div>
         </div>
     </section>
-    <!-- /.content -->
   </div>
 </template>
 
@@ -37,50 +31,32 @@
 
 export default {
   name: 'TaskList',
-  data() {
-    return {
-      taskList: taskListSample
-    }
-  },
   computed: {
     taskList() {
-      // console.log(taskListSample);
-      return taskListSample;
+      console.log(`task list: ${this.$store.getters.tasks}`);
+      return this.$store.getters.tasks.filter(t => !(t.approvedBy && t.completedBy));
+    },
+    isParent: function(){
+      let currentUser = this.$store.getters.currentUser;      
+      if(currentUser){
+        console.log(`Is Parent called: ${this.$store.getters.currentUser.isPrimary}`);
+        return this.$store.getters.currentUser.isPrimary;
+      }
+      return false;
     }
   },
-  created(){
-    // this.$store.dispatch('connectRoom', 'sample');
+  methods: {
+    completeTask(taskId){
+      // let taskId = event.target.value;
+      console.log(`TaskId: `+taskId);
+      this.$store.dispatch('completeTask', taskId);
+    },
+    approveTask(taskId){
+      console.log(`TaskId: `+taskId);
+      this.$store.dispatch('approveTask', taskId);
+    }
   }
 };
-
-const taskListSample = [
-  {
-    id: 0,
-    description: "This is a task",
-    reward: 5
-  },
-  {
-    id: 0,
-    description: "This is a task",
-    reward: 5
-  },
-  {
-    id: 1,
-    description: "This is a task",
-    reward: 5
-  },
-  {
-    id: 2,
-    description: "This is a task",
-    reward: 5
-  },
-  {
-    id: 3,
-    description: "This is a task",
-    reward: 5
-  }
-]
-
 
 </script>
 
